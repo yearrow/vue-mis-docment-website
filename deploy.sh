63ff86b34441d01eefd683e7e9f8c0e18b79ab07
danielmlc <danielmlc@126.com>
2020-02-04 21:10:57 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@ set -e
 # 生成静态文件
 npm run build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,6 +15,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<danielmlc>/<danielmlc>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:danielmlc/blog.git master:gh-pages
+git push -f git@github.com:YLSoftWorkGroup/YlDocument.git master 
 
 cd - 
