d736e9c697f9f50fc08c25a7c61c59d457a7a614
danielmlc <danielmlc@126.com>
2020-02-05 16:38:41 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<danielmlc>/<danielmlc>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YLSoftWorkGroup/YlDocument.git master :gh-pages 
+git push -f git@github.com:YLSoftWorkGroup/YlDocument.git master:gh-pages 
 
 cd -
 
