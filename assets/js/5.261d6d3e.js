6e3f3a7f94872cc80d925e213b4568f662f559d2
danielmlc <danielmlc@126.com>
2021-01-04 20:47:22 +0800

    deploy

---
+++ assets/js/5.261d6d3e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{457:function(t,s,e){"use strict";e.r(s);var n=e(14),i=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("img",{attrs:{src:this.$withBase("/img/bg.png"),width:"100%"}})])}),[],!1,null,null,null);s.default=i.exports}}]);
